e5685719e3a4866b69a3a698ae43e237342da9a7
Neha Sharma <neha.iitjdh@gmail.com>
2025-07-27 11:35:00 +0530

    Initial Deployment Checkup

--- conversaflow/src/App.tsx
+++ conversaflow/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - Neha Sharma
         </a>
       </header>
     </div>
