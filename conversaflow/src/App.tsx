4ccfe792379426fc17a058ccdb2969117f20f97f
Neha Sharma <neha.iitjdh@gmail.com>
2025-08-01 15:00:00 +0530

    Update App Screen

--- conversaflow/src/App.tsx
+++ conversaflow/src/App.tsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components/common/Layout';
 import { Overview } from '@/pages/Overview';
+import { Performance } from '@/pages/Performance';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Overview />} />
-        <Route path="/performance" element={<div>Performance Page</div>} />
+        <Route path="/performance" element={<Performance />} />
         <Route path="/activity" element={<div>Activity Page</div>} />
         <Route path="/product" element={<div>Product Page</div>} />
         <Route path="/task" element={<div>Task Page</div>} />
