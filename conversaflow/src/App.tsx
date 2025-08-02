a94cb5ee5908ab2d8d912f66793f79df340584e8
Neha Sharma <neha.iitjdh@gmail.com>
2025-08-02 19:00:00 +0530

    Add Product to App

--- conversaflow/src/App.tsx
+++ conversaflow/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components/common/Layout';
 import { Overview } from '@/pages/Overview';
 import { Performance } from '@/pages/Performance';
+import { Product } from '@/pages/Product';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<Overview />} />
         <Route path="/performance" element={<Performance />} />
         <Route path="/activity" element={<div>Activity Page</div>} />
-        <Route path="/product" element={<div>Product Page</div>} />
+        <Route path="/product" element={<Product />} />
         <Route path="/task" element={<div>Task Page</div>} />
       </Routes>
     </Layout>
