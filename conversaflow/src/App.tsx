2a3f786e7959b23723f475b303b3447978fde71c
Neha Sharma <neha.iitjdh@gmail.com>
2025-08-03 16:00:00 +0530

    Update App with all routes

--- conversaflow/src/App.tsx
+++ conversaflow/src/App.tsx
@@ -2,7 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from '@/components/common/Layout';
 import { Overview } from '@/pages/Overview';
 import { Performance } from '@/pages/Performance';
+import { Activity } from '@/pages/Activity';
 import { Product } from '@/pages/Product';
+import { Task } from '@/pages/Task';
 
 function App() {
   return (
@@ -10,9 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Overview />} />
         <Route path="/performance" element={<Performance />} />
-        <Route path="/activity" element={<div>Activity Page</div>} />
+        <Route path="/activity" element={<Activity />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/task" element={<div>Task Page</div>} />
+        <Route path="/task" element={<Task />} />
       </Routes>
     </Layout>
   );
