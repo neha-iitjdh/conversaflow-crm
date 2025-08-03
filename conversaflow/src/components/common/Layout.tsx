2c6e748980ac1bdbd746f6d0089159cb2eed92bb
Neha Sharma <neha.iitjdh@gmail.com>
2025-08-03 15:00:00 +0530

    Refine Layout Page

--- conversaflow/src/components/common/Layout.tsx
+++ conversaflow/src/components/common/Layout.tsx
@@ -1,18 +1,49 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
-import { Header } from "./Header";
-import { NavTabs } from "./NavTabs";
+// import React from "react";
+// import { Box, Container } from "@mui/material";
+// import { Header } from "./Header";
+// import { NavTabs } from "./NavTabs";
+
+// interface LayoutProps {
+//   children: React.ReactNode;
+// }
+
+// export const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   return (
+//     <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+//       <Header />
+//       <Box sx={{ mt: 8, flexGrow: 1 }}>
+//         <Container maxWidth="xl" sx={{ py: 3 }}>
+//           <NavTabs />
+//           {children}
+//         </Container>
+//       </Box>
+//     </Box>
+//   );
+// };
+import React from 'react';
+import { Box, Container, useMediaQuery, useTheme } from '@mui/material';
+import { Header } from './Header';
+import { NavTabs } from './NavTabs';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Header />
       <Box sx={{ mt: 8, flexGrow: 1 }}>
-        <Container maxWidth="xl" sx={{ py: 3 }}>
+        <Container 
+          maxWidth="xl" 
+          sx={{ 
+            py: 3,
+            px: isMobile ? 2 : 3,
+          }}
+        >
           <NavTabs />
           {children}
         </Container>
